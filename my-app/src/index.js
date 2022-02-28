cfc21bb7aeeb2b33d9b4402f1277bbe8cd0b0865
Dong Huang Chen <81045036+dong200k@users.noreply.github.com>
2022-02-28 17:25:09 -0500

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Login from "./pages/login/login.jsx"
-import Home from "./pages/home/home.jsx"
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />}/>
-        </Routes>
-      </BrowserRouter>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
