e12b502e39a34dfef2d92c0393f224f480922eda
Dong Huang Chen <81045036+dong200k@users.noreply.github.com>
2022-03-02 11:00:56 -0500

    change backend

---
+++ my-app/src/component/account.jsx
@@ -0,0 +1,8 @@
+import render from "react"
+
+export default function AccountKids(){
+    return <div>
+            <h1>Account/Kids Page</h1>
+            
+        </div>
+}
---
+++ my-app/src/component/program.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom"
+export default function Program(){
+    return <div>
+        <h1>Program Page!</h1>
+        <Outlet/>
+    </div>
+    
+}
