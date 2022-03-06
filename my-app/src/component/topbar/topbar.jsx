b8dd88a9e5cdb11adac3c62c4896e0526bedeec9
Lun <71906158+Lunn99@users.noreply.github.com>
2022-03-06 12:39:08 -0500

    Topbar

--- my-app/src/component/topbar/topbar.jsx
+++ my-app/src/component/topbar/topbar.jsx
@@ -7,10 +7,7 @@ export default class topbar1 extends Component {
     };
     render() {
         return (
-            <header className="topHeader">
-                <div className="topLogo"> 
-                    <img className="topImg" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></img>
-                </div>
+            <div className="topbar">
                 <div className={`topToggle ${this.state.isUnfold ? 'close' : ''}`}
                 onClick={() => {
                     this.setState({
@@ -30,12 +27,15 @@ export default class topbar1 extends Component {
                         <li className="topListItem"><span>Message</span></li>
                     </ul>
                 </div>
+                <div className={`topLogo ${this.state.isUnfold ? '' : 'active'}`}>
+                    Youth Sport Center
+                </div>
                 <div className="topProfile">
                 {
                     <img className="topImg" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></img>
                 }
                 </div>
-            </header>
+            </div>
         )
     }
 }
