5567d7029a4ed04d61eb5cfe1e44424c9e020e64
Dong Huang Chen <81045036+dong200k@users.noreply.github.com>
2022-04-26 18:12:38 -0400

    make it better

--- my-app/src/services/baseURL.js
+++ my-app/src/services/baseURL.js
@@ -1,2 +1,3 @@
-const baseURL = "http://localhost:5000/api/v1/youthsportcenter"
+// const baseURL = "http://localhost:5000/api/v1/youthsportcenter"
+const baseURL = "https://sproj-youth-sport-center.herokuapp.com/api/v1/youthsportcenter"
 export default baseURL
